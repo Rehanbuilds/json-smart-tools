fa3a43b4c6e1a450b9a446b8b6fc8faade26e566
Mr Rehan <rehanmatloob43@gmail.com>
2026-01-01 20:16:00 +0500

    Add JSON validation function

---
+++ validator.ts
@@ -0,0 +1,13 @@
+import { ValidationResult } from './types'
+
+export function validateJson(input: string): ValidationResult {
+  try {
+    JSON.parse(input)
+    return { valid: true }
+  } catch (error: any) {
+    return {
+      valid: false,
+      message: error.message
+    }
+  }
+}
